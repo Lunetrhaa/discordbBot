c1f606c77023c25edd4922fa756c291cfe6bef93
Lunetrhaa <malfjaaa@gmail.com>
2026-05-10 20:32:01 +0700

    Update sholat.py

--- bot/sholat.py
+++ bot/sholat.py
@@ -8,7 +8,7 @@ import discord
 from discord import app_commands
 from discord.ext import commands, tasks
 
-import config as cfg
+from . import config as cfg
 
 log = logging.getLogger("bot.sholat")
 
