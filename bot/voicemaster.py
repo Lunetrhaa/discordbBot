c0aef25b9c1bde1d63690e0c0c363ef02f3503c3
Lunetrhaa <malfjaaa@gmail.com>
2026-05-10 20:34:16 +0700

    Update voicemaster.py

--- bot/voicemaster.py
+++ bot/voicemaster.py
@@ -6,7 +6,7 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
-import config as cfg
+from . import config as cfg
 
 log = logging.getLogger("bot.voicemaster")
 
